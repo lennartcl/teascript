9cea984a073be29efad113d9d965b5cca6b4de66
Lennart Kats <lclkats@gmail.com>
2015-11-15 15:16:32 +0000

    Add additional options from jstransform

--- teascript.js
+++ teascript.js
@@ -12,10 +12,56 @@ var args = optimist
     .describe('d', 'Decompile a file')
     .alias('h', 'help')
     .describe('help', 'Show help')
+    .describe(
+      '--react',
+      'Turns on the React JSX and React displayName transforms'
+    ).describe(
+      '--es6',
+      'Turns on available ES6 transforms'
+    ).describe(
+      '--es7',
+      'Turns on available ES7 transforms'
+    ).describe(
+      '--harmony',
+      'Shorthand to enable all ES6 and ES7 transforms'
+    ).describe(
+      '--utility',
+      'Turns on available utility transforms'
+    ).describe(
+      '--target [version]',
+      'Specify your target version of ECMAScript. Valid values are "es3" and ' +
+      '"es5". The default is "es5". "es3" will avoid uses of defineProperty and ' +
+      'will quote reserved words. WARNING: "es5" is not properly supported, even ' +
+      'with the use of es5shim, es5sham. If you need to support IE8, use "es3".',
+      'es5'
+    /* Enabled by default:
+    ).describe(
+      '--strip-types',
+      'Strips out type annotations.'
+    */
+    ).describe(
+      '--es6module',
+      'Parses the file as a valid ES6 module. ' +
+      '(Note that this means implicit strict mode)'
+    ).describe(
+      '--non-strict-es6module',
+      'Parses the file as an ES6 module, except disables implicit strict-mode. ' +
+      '(This is useful if you\'re porting non-ES6 modules to ES6, but haven\'t ' +
+      'yet verified that they are strict-mode safe yet)'
+    /* Not supported:
+    ).describe(
+      '--source-map-inline',
+      'Embed inline sourcemap in transformed source'
+    */
+    ).describe(
+      '--source-filename',
+      'Filename to use when generating the inline sourcemap. Will default to ' +
+      'filename when processing files'
+    )
     .argv;
 
 if (args.help || (!args.compile && !args.decompile)) {
-    args.showHelp();
+    optimist.showHelp();
     process.exit();
 }
 
@@ -29,10 +75,9 @@ if (args.decompile) {
     result = transform.unpackString(file);
 }
 else if (args.compile) {
-    var compiled = jstransform.transform(file, {
-        stripTypes: true,
-        es6: false
-    });
+    if (args.stripTypes === undefined)
+        args.stripTypes = true;
+    var compiled = jstransform.transform(file, args);
     result = transform.packStrings(file, compiled.code, { stripSpaces: true });
 }
 
